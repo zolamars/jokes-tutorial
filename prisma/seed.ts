09b9c00106c0ec25dd4c0f548bc952109eb53266
@zolabera <zolyemek@gmail.com>
2023-07-28 16:22:51 +0300

    Connect to the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -49,7 +49,7 @@ function getJokes() {
     },
     {
       name: "Skeletons",
-      content: `Why don’t skeletons ever go trick or treating? Because they have nobody to go with.`,
+      content: `Why don't skeletons ever go trick or treating? Because they have nobody to go with.`,
     }
   ];
 }
