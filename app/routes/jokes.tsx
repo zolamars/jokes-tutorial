30d5e6808f4385e308c08e20283d4dde198bed3c
@zolabera <zolyemek@gmail.com>
2023-07-29 09:43:39 +0300

    Read from the database  in a remix loader

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,13 +1,27 @@
 import type { LinksFunction } from "@remix-run/node";
-import { Link, Outlet } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import {
+  Link,
+  Outlet,
+  useLoaderData,
+} from "@remix-run/react";
 
 import stylesUrl from "~/styles/jokes.css";
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
 ];
 
+export const loader = async () => {
+  return json({
+    jokeListItems: await db.joke.findMany(),
+  });
+};
+
 export default function JokesRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -30,9 +44,11 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              <li>
-                <Link to="some-joke-id">Hippo</Link>
-              </li>
+              {data.jokeListItems.map(({ id, name }) => (
+                <li key={id}>
+                  <Link to={id}>{name}</Link>
+                </li>
+              ))}
             </ul>
             <Link to="new" className="button">
               Add your own
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -49,7 +49,7 @@ function getJokes() {
     },
     {
       name: "Skeletons",
-      content: `Why don't skeletons ever go trick or treating? Because they have nobody to go with.`,
+      content: `Why do not skeletons ever go trick or treating? Because they have nobody to go with.`,
     }
   ];
 }
