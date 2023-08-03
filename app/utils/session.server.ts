a476a4cd627fb78363099509f9fab862be922c5f
@zolabera <zolyemek@gmail.com>
2023-08-03 17:45:46 +0300

    User registration

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -11,6 +11,17 @@ type LoginForm = {
   username: string;
 };
 
+export async function register({
+  password,
+  username,
+}: LoginForm) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = await db.user.create({
+    data: { passwordHash, username },
+  });
+  return { id: user.id, username };
+}
+
 export async function login({
   password,
   username,
